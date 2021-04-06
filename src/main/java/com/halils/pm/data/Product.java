ae6f1531ac0f204c9b5037491e11493302926a8b
bitpixeled <bitpixeled@gmail.com>
2021-04-06 22:58:51 +0300

    Improvement

--- src/main/java/com/halils/pm/data/Product.java
+++ src/main/java/com/halils/pm/data/Product.java
@@ -47,31 +47,50 @@ public class Product {
     private int id;
     private String name;
     private BigDecimal price;
+    private final Rating rating;
 
+    public Product() {
+        this(0, "No Name", BigDecimal.ZERO);
+    }
+
+    public Product(int id, String name, BigDecimal price) {
+        this(id, name, price, Rating.NOT_RATED);
+    }
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+
+    public Rating getRating() {
+        return rating;
+    }
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+//    public void setId(int id) {
+//        this.id = id;
+//    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
+//    public void setPrice(BigDecimal price) {
+//        this.price = price;
+//    }
 
     /**
      * Calculates discount based on product price and
@@ -81,5 +100,9 @@ public class Product {
      */
     public BigDecimal getDiscount(){
         return price.multiply(DISCOUNT_RATE).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public Product applyRating (Rating newRating){
+        return new Product(id, name, price, newRating);
     }
 }
